929a40b604b2d13fbced4fe46ddcb373ad03528e
Shubham Bhardwaj <shubham2000bhardwaj@gmail.com>
2021-07-19 16:42:51 +0530

    Adding backend functionality to docker page

--- FrontEnd/form.js
+++ FrontEnd/form.js
@@ -3,5 +3,17 @@ function Launch_Container(){
   document.getElementById("launch_new_container").style.display = "block"};
 
 function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+  document.getElementById("launch_new_container").style.display = "none";
+}
+function Stop_Container(){
+  document.getElementById("stop_running_container").style.display = "block"};
+
+function closeForm() {
+  document.getElementById("stop_running_container").style.display = "none";
+}
+function Temp_Container(){
+  document.getElementById("run_command").style.display = "block"};
+
+function closeForm() {
+  document.getElementById("run_command").style.display = "none";
 }
