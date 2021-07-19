b58b05150b21dffed158d9cd37f6722ecf85c7cd
Shubham Bhardwaj <shubham2000bhardwaj@gmail.com>
2021-07-19 15:09:44 +0530

    adding pop input box in docker page

---
+++ FrontEnd/form.js
@@ -0,0 +1,7 @@
+
+function Launch_Container(){
+  document.getElementById("launch_new_container").style.display = "block"};
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
