2052951f4867feb0813ddb9b2bd194d403295c3f
Shubham Bhardwaj <shubham2000bhardwaj@gmail.com>
2021-07-19 17:20:19 +0530

    Added js fns for docker buttons

--- FrontEnd/form.js
+++ FrontEnd/form.js
@@ -2,18 +2,35 @@
 function Launch_Container(){
   document.getElementById("launch_new_container").style.display = "block"};
 
-function closeForm() {
+function Launch_Container_Close_Form() {
   document.getElementById("launch_new_container").style.display = "none";
 }
 function Stop_Container(){
   document.getElementById("stop_running_container").style.display = "block"};
 
-function closeForm() {
+function Stop_Container_Close_Form() {
   document.getElementById("stop_running_container").style.display = "none";
 }
+
+
+function Remove_Container(){
+  document.getElementById("remove_container").style.display = "block"};
+
+function Remove_Container_Close_Form() {
+  document.getElementById("remove_container").style.display = "none";
+}
+
+function Remove_Image(){
+  document.getElementById("remove_image").style.display = "block"};
+
+function Remove_Image_Close_Form() {
+  document.getElementById("remove_image").style.display = "none";
+}
+
+
 function Temp_Container(){
   document.getElementById("run_command").style.display = "block"};
 
-function closeForm() {
+function Temp_Container_Close_Form() {
   document.getElementById("run_command").style.display = "none";
 }
